a8c9ffd2ddf6dd121f6691ea2a6f59b384dca329
Oleksandr Khotemskyi <xotabu4@gmail.com>
2020-11-05 19:23:00 +0200

    Update createAccount.page.ts

--- page/createAccount.page.ts
+++ page/createAccount.page.ts
@@ -11,6 +11,8 @@ export class CreateAccountPO {
         this.registrationForm.$('input[name="lastname"]').setValue(options.lastname)
         const countrySelect = this.registrationForm.$('select[name="country_code"]')
         countrySelect.selectByVisibleText(options.countryName)
+        this.registrationForm.$('input[name="email"]').click()
+        browser.pause(50)
         this.registrationForm.$('input[name="email"]').setValue(options.email)
         this.registrationForm.$('input[name="phone"]').setValue(options.phone)
 
