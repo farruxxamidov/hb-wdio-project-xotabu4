d2de741c8810b47237718f878c6e8f98aa03afe1
Oleksandr Khotemskyi <xotabu4@gmail.com>
2020-11-05 22:16:44 +0200

    Added allure report

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -20,11 +20,26 @@ export const config: Config = {
     connectionRetryTimeout: 120000,
     connectionRetryCount: 3,
     framework: 'mocha',
-    reporters: ['spec'],
+    reporters: ['spec',
+        [
+            "allure",
+            {
+                outputDir: "allure-results",
+                disableMochaHooks: true,
+                disableWebdriverStepsReporting: true,
+                disableWebdriverScreenshotsReporting: false
+            }
+        ]
+    ],
     mochaOpts: {
         ui: 'bdd',
         timeout: 60000,
         // retries: 3,
         // grep: '@SMOKE'
+    },
+    afterTest: function (test) {
+        if (test.error !== undefined) {
+            browser.takeScreenshot();
+        }
     }
 }
